b8aadc4456abb50abc9e4e2dfcd3fc263c883eed
Richard Carranza <rishardcarranza@gmail.com>
2019-02-21 22:02:36 -0600

    Added new functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClient,
   ],
   providers: [],
   bootstrap: [AppComponent]
